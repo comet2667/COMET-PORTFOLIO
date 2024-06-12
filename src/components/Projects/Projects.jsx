92d207ae0eee9cdad4cd11e96c5518e7210029b7
comet <comet2667@naver.com>
2024-06-12 21:56:13 +0900

    프로젝트 내용 추가

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,11 +1,51 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './Projects.scss';
 
-function Projects(){
-    return(
+import indie1 from '../../assets/project/indiean/indie_1.png';
+import indie2 from '../../assets/project/indiean/indie_2.png';
+import indie3 from '../../assets/project/indiean/indie_3.png';
+import indie4 from '../../assets/project/indiean/indie_4.png';
+
+function Projects() {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    }
+
+    return (
         <div className='projects__container'>
             <h1>Projects</h1>
             <div className='projects__box'>
-                
+                <h2>Indie:안</h2>
+                <div className='projects__slide'>
+                    <Slider {...settings}>
+                        <img src={indie1} />
+                        <img src={indie2} />
+                        <img src={indie3} />
+                        <img src={indie4} />
+                    </Slider>
+                </div>
+                <div className='projects__content'>
+                    <div className='projects__left'>
+                        <b>마케팅 비용을 충당하기 어려운 소자본/무자본 아티스트들의 크라우드 펀딩을 위한 웹사이트 입니다.</b><br /><br />
+                        매체에 노출되지 못하는 실력있는 아티스트들과 해당 아티스트들의<br /> 공연, 앨범을 원하는 팬 및 리스너들의 이해관계를 충족시켜보고자<br /> 기획하였습니다.<br /><br />
+                        개발환경 구축부터 배포까지, 개발 전체 과정을 직접 관리해볼 수 있는 좋은 경험이었습니다.<br /><br />
+                        팀장으로서 JPA와 같은 새로운 기술을 도입한 것은 좋았으나<br />
+                        팀원들에게 해당 기술을 사용하는 이유와 발생할 수 있는 문제점에 대해 제대로 전달하지 못하여 개발이 지연되고
+                        코드 작성 패턴이 통일되지<br /> 못한 부분은 아쉬웠습니다.<br /><br />
+                        해당 프로젝트는 학습을 위한 프로젝트인만큼 현재까지도 오류수정 및 리팩토링을 진행하고 있습니다.
+                    </div>
+                    <div className='projects__line'></div>
+                    <div className='projects__right'>
+                        내용입니다.
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -27,8 +27,7 @@ import figmaLogo from '../../assets/logo/figma_logo.png';
 import flowLogo from '../../assets/logo/flow_logo.png';
 
 function Skills() {
-    //Frontend, Backend, Deployment, Version Control, Communication
-    //BE = java, springBoot, JPA, Oracle
+
     return (
         <div className="skills__container">
             <h1>Skills</h1>
@@ -38,31 +37,31 @@ function Skills() {
                     <img src={javaLogo} />
                     <div className='skills__line'></div>
                     <div>
-                        자바 관련 내용
+                        개발 기초 공부부터 프로젝트까지 전부 자바로 진행하였습니다.<br />
+                        현재는 최적화 및 클린코드 작성 훈련을 위해 팀 프로젝트 팀원들과 리팩토링을 진행하고 있습니다.
                     </div>
                 </div>
                 <div className='skills__item'>
                     <img src={springBootLogo} />
                     <div className='skills__line'></div>
                     <div>
-                        스프링부트 관련 내용
-                    </div>
-                </div>
-                <div className='skills__item'>
-                    <img src={oracleLogo} />
-                    <div className='skills__line'></div>
-                    <div>
-                        오라클 관련 내용
+                        스프링 학습 이후, 2건의 팀프로젝트와 공공데이터 활용 및 openAPI 학습을 스프링부트로 진행하였습니다.<br />
+                        팀장을 맡은 프로젝트에선 ResponseEntity와 CutomException을 활용하여 통일된 형식의 API 응답을 구현해보았습니다.
                     </div>
                 </div>
                 <div className='skills__item'>
                     <img src={jpaLogo} />
                     <div className='skills__line'></div>
                     <div>
-                        JPA 관련 내용
+                        학습과 동시에 최근 프로젝트에 적용하였습니다.<br />
+                        학습 초기에는 편의성만을 고려하였으나 엔티티의 연관관계 매핑부터 Join 이슈 까지, 사용할 수록 고려해야될 부분이 많았습니다.<br />
+                        하지만 배포 과정 중 DB 벤더를 변경하거나 유지보수 면에서 JPA의 장점을 체감할 수 있었습니다.
                     </div>
                 </div>
                 <div className='skills__etc'>
+                    <div>
+                        <img src={oracleLogo} />
+                    </div>
                     <div>
                         <img src={psqlLogo} />
                     </div>
@@ -81,29 +80,26 @@ function Skills() {
                     <img src={htmlLogo} />
                     <div className='skills__line'></div>
                     <div>
-                        html 관련 내용
+                        반응형 까지 고려하지는 못하나 특정 요소를 원하는 위치에 배치하거나 특정 조건에 따라 동작을 제어할 수 있습니다.
                     </div>
                 </div>
                 <div className='skills__item'>
                     <img src={reactLogo} />
                     <div className='skills__line'></div>
                     <div>
-                        react 관련 내용
-                    </div>
-                </div>
-                <div className='skills__item'>
-                    <img src={recoilLogo} />
-                    <div className='skills__line'></div>
-                    <div>
-                        recoil 관련 내용
+                        2건의 팀 프로젝트 프론트엔드 개발을 React+Vite 기반 환경에서 진행하였습니다.<br />
+                        공통으로 사용되는 요소를 컴포넌트로 분리할 수 있고 Axios를 통해 동적 웹을 구현할 수 있습니다.
                     </div>
                 </div>
                 <div className='skills__etc'>
                     <div>
+                        <img src={recoilLogo} />
+                    </div>
+                    <div>
                         <img src={viteLogo} />
                     </div>
                     <div>
-                        <img src={axiosLogo}/>
+                        <img src={axiosLogo} />
                     </div>
                     <div>
                         <img src={sassLogo} />
@@ -117,7 +113,8 @@ function Skills() {
                     <img src={awsLogo} />
                     <div className='skills__line'></div>
                     <div>
-                        aws 관련 내용
+                        최근 진행한 팀 프로젝트인 '인디안'을 AWS EC2 인스턴스를 통해 배포하였습니다.<br/>
+                        기능별 이미지 CRUD는 S3 버킷에 임시 이미지와 저장 이미지를 구분하여 처리하였고 RDS를 통해 PostGreSQL 데이터베이스를 생성하였습니다.<br/>
                     </div>
                 </div>
                 <div className='skills__etc'>
@@ -125,7 +122,7 @@ function Skills() {
                         <img src={ec2Logo} />
                     </div>
                     <div>
-                        <img src={rdsLogo}/>
+                        <img src={rdsLogo} />
                     </div>
                     <div>
                         <img src={s3Logo} />
@@ -139,7 +136,8 @@ function Skills() {
                     <img src={gitLogo} />
                     <div className='skills__line'></div>
                     <div>
-                        github 관련 내용
+                        팀 프로젝트 전부 github를 통해 진행하였습니다.<br/>
+                        팀장 역할을 맡아 팀원들의 Pull Request를 리뷰하고 main branch를 관리한 경험이 있습니다.
                     </div>
                 </div>
             </div>
@@ -151,7 +149,7 @@ function Skills() {
                         <img src={figmaLogo} />
                     </div>
                     <div>
-                        <img src={flowLogo}/>
+                        <img src={flowLogo} />
                     </div>
                     <div>
                         <img src={notionLogo} />
